5a1bfea07d6490432a6751513286802bb72d234b
Reesta <marko.reestic@gmail.com>
2016-04-12 22:14:22 +0200

    Added special effects

---
+++ Assets/Scripts/AttachToCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Utility;
+
+public class AttachToCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+	    foreach(var cam in Camera.allCameras)
+        {
+            Debug.Log("cam " + cam.name);
+            cam.GetComponent<SmoothFollow>().target = gameObject.transform;
+        }
+	}
+}
